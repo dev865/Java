20dc40954cff64b15a73bedff1eed3db29b0a68a
debugger <parapet-origami.0o@icloud.com>
2023-03-14 19:34:46 +0900

    Update Ex01.java

--- manager/src/manager/Ex01.java
+++ manager/src/manager/Ex01.java
@@ -14,9 +14,12 @@ public class Ex01 {
 		
 		try {
 			// 1. 드라이버로딩
+			// Class.forName("driver") 를 하게되면 DriverManager에 등록되게 된다.
 			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("클래스 로딩 완료!");
 			
-			// 2. 연결하기
+			// 2. 연결하기(접속하기)
+			// DriverManager.getConnection("url", "user", "password")
 			String url = "jdbc:mysql://localhost/java review db";
 			conn = DriverManager.getConnection(url, "javauser", "mysql");
 			
@@ -33,6 +36,7 @@ public class Ex01 {
 			rs = stmt.executeQuery(sql);
 			
 			// 6,실행결과 출력하기
+			// rs.next는 다음 레코드로 커서를 옮기는것
 			while(rs.next()) {
 				// 레코드의 칼럼은 배열과 달리 0부터 시작하지않고 1부터 시작한다.
 				// 가져오는 데이터의 형태에 맞게 getString, getInt 등 사용
@@ -48,6 +52,29 @@ public class Ex01 {
 		}
 		catch(SQLException e) {
 			System.out.println("에러 : " + e);
+		}
+		finally {
+			if(rs != null) {
+				try {
+					rs.close();
+				} catch(SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(stmt != null) {
+				try {
+					stmt.close();					
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(conn != null) {
+				try {
+					conn.close();					
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 }
