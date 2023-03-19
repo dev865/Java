fff40018f4a4028f6ce7e8b26da8c19d8da944e0
debugger <parapet-origami.0o@icloud.com>
2023-03-19 20:45:21 +0900

    init JdbcConnectionUtil

---
+++ manager/src/manager/JdbcConnectionUtil.java
@@ -0,0 +1,63 @@
+package manager;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+// 싱글톤 패턴 적용
+public class JdbcConnectionUtil {
+	private static JdbcConnectionUtil instance;
+	
+	private String url = "jdbc:mysql://localhost/java review db";
+	private String user = "javauser";
+	private String password = "mysql";
+	
+	private JdbcConnectionUtil() {
+		
+	}
+	
+	public static JdbcConnectionUtil getInstance() {
+		synchronized(JdbcConnectionUtil.class) {
+			if(instance == null) {
+				instance = new JdbcConnectionUtil();
+			}
+		}
+		return instance;			
+	}
+	
+	public Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(url, user, password);
+	}
+	
+	public void close(ResultSet rs) {
+		if(rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void close(Statement stmt) {
+		if(stmt != null) {
+			try {
+				stmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void close(Connection con) {
+		if(con != null) {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
